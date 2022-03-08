c2ec1d26dcec3379fb34bcf5f047ea7064dfe3ec
ali <aliemamalinezhad@gmail.com>
2022-03-08 19:24:22 +0330

    create ProductSerializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,20 @@
-from django.http import JsonResponse
-import json
 from django.forms.models import model_to_dict
-from products.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
 
-    model_data = Product.objects.all().order_by('?').first()
+    
+    instanse = Product.objects.all().order_by('?').first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data)
+    if instanse:
+        data = ProductSerializer(instanse).data
 
-    return JsonResponse(data)
+    return Response(data)
     
  
